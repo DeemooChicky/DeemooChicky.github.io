b7f83fca4ecd7027c8fa288dbfab10c9916d14d6
DeemooChicky <111826320+DeemooChicky@users.noreply.github.com>
2022-09-03 11:59:52 +0800

    hehe

---
+++ script.js
@@ -0,0 +1,21 @@
+var a=prompt("Mahal mo ba ako? (small letters english)");
+	if(a=="yes")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="NASA.html";
+  	}
+ var b=prompt("you're gonna be my?");
+  	if(b=="wife")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="NASA.html";
+  	}
+  	
